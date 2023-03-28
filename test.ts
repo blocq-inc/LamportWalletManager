17093ab2e29a5d07d003af35caa95a400071b3d4
william_doyle <mcbidebilly@gmail.com>
2023-03-28 17:52:38 -0400

    simple demo of inheritance

--- test.ts
+++ test.ts
@@ -12,4 +12,26 @@
 // console.log(`n_one --> `, n_one.toString('hex'))
 // console.log(`n_two --> `, ethers.BigNumber.from(n_two).toHexString())
 
+
+import BaseKeyTracker from './BaseKeyTracker'
+import KeyTrackerA from './KeyTrackerA'
+import KeyTrackerB from './KeyTrackerB'
+import KeyTrackerC from './KeyTrackerC'
+
+const keyTrackers : BaseKeyTracker[] = [
+    new KeyTrackerA(),
+    new KeyTrackerB(),
+    new KeyTrackerC(),
+]
+
 console.log(`TEST KEY TRACKERS`)
+
+const showCounts = () => keyTrackers.forEach((keyTracker, index) => console.log(`${index}. count --> ${keyTracker.count}`))
+showCounts()
+
+keyTrackers.forEach((keyTracker, index) => keyTracker.more(10))
+showCounts()
+
+
+
+
